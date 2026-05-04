137835ebc15f9b768b9356b9b6efbe80f85d5440
MPeets <mihkelpeets65@gmail.com>
2026-05-04 15:37:27 +0300

    feat(infra): add GitLab OIDC deployer role trust
    Create the bootstrap IAM identity provider and dev/prod deployer roles so GitLab CI can assume AWS roles through project-scoped OIDC trust.

--- modules/bootstrap-backend/main.tf
+++ modules/bootstrap-backend/main.tf
@@ -51,3 +51,64 @@ resource "aws_dynamodb_table" "locks" {
 
   tags = var.tags
 }
+
+data "tls_certificate" "gitlab" {
+  url = "https://gitlab.com"
+}
+
+resource "aws_iam_openid_connect_provider" "gitlab" {
+  url = "https://gitlab.com"
+
+  client_id_list = [
+    "https://gitlab.com",
+  ]
+
+  thumbprint_list = [
+    for certificate in data.tls_certificate.gitlab.certificates : certificate.sha1_fingerprint
+  ]
+
+  tags = var.tags
+}
+
+data "aws_iam_policy_document" "gitlab_deployer_assume_role" {
+  statement {
+    actions = [
+      "sts:AssumeRoleWithWebIdentity",
+    ]
+
+    principals {
+      type = "Federated"
+      identifiers = [
+        aws_iam_openid_connect_provider.gitlab.arn,
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "gitlab.com:aud"
+      values = [
+        "https://gitlab.com",
+      ]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "gitlab.com:sub"
+      values = [
+        "project_path:${var.gitlab_project_path}:*",
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "dev_deployer" {
+  name               = var.dev_deployer_role_name
+  assume_role_policy = data.aws_iam_policy_document.gitlab_deployer_assume_role.json
+  tags               = var.tags
+}
+
+resource "aws_iam_role" "prod_deployer" {
+  name               = var.prod_deployer_role_name
+  assume_role_policy = data.aws_iam_policy_document.gitlab_deployer_assume_role.json
+  tags               = var.tags
+}
